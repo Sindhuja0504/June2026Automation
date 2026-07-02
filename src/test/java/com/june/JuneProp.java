a3da08b83166c532b122e4e0ee1dcb428fcbeb37
Kannan M. <M758@eaglesoftware.com>
2026-07-02 12:06:55 +0530

    added sop

--- src/test/java/com/june/JuneProp.java
+++ src/test/java/com/june/JuneProp.java
@@ -6,6 +6,7 @@ import java.util.Properties;
 
 public class JuneProp {
 	public static void prop() {
+		System.out.println("true = " + true);
 		try {
 			FileReader file = new FileReader(
 					"C:\\Users\\M1001\\projectclss-workspace\\JuneProject\\src\\test\\resources\\Files\\sheet.properties");
